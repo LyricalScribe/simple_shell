11e8bf2078ad5f34f281d2629b9d72c398673c58
Thulisile <111003983+LyricalScribe@users.noreply.github.com>
2023-06-16 19:15:11 +0200

    reworked shell scripts

--- _getline.c
+++ _getline.c
@@ -1,142 +1,125 @@
 #include "shell.h"
-
 /**
- * salida3 - normal out
- * @m: copy of environmental variables
- * @e: number of elements in m
+ * _getline - prints "$ ", wait for the user to enter a command, prints it on
+ *  the next line
+ * @input: pointer to buffer to store input
+ * @size: size of a buffer
+ * Return: chars read or -1 if _realloc failed
  */
-void salida3(char **m, int e)
+int _getline(char *input, int size)
 {
-	free_grid(m, e);
-	exit(currentstatus(NULL));
-}
+	int i;
+	int readval;
+	struct stat sb;
 
-/**
- * salida2 - out with double Ctrl+D
- * @m: copy of environmental variables
- * @e: number of elements in m
- * @line: input of user
- */
-void salida2(char **m, int e, char *line)
-{
-	free(line);
-	free_grid(m, e);
-	write(STDIN_FILENO, "#cisfun$ ", 9);
-	write(STDIN_FILENO, "\n", 1);
-	exit(currentstatus(NULL));
-}
-
-/**
- * salida1 - normal out
- * @m: copy of environmental variables
- * @e: number of elements in m
- */
-void salida1(char **m, int e)
-{
-	free_grid(m, e);
-	write(STDIN_FILENO, "\n", 1);
-	exit(currentstatus(NULL));
-}
-
-/**
- * _getline - function to read what the user writes
- * @a: pointer to loop counter
- * @e: length of m
- * @m: copy of environmental
- * Return: line in sucess otherwise NULL.
- */
-char  *_getline(int *a, char **m, int e)
-{
-	char letter[1] = {0}, *line = NULL;
-	size_t bufsize = 0;
-	static int num = 1;
-
-	if (num == 2)
-		salida2(m, e, line);
-	for (; (num != 0); bufsize = 0, free(line))
+	if (fstat(STDIN_FILENO, &sb) == -1)
 	{
-		write(STDIN_FILENO, "#cisfun$ ", 9);
-		*a = *a + 1;
-		signal(SIGINT, _signal);
-		for (; ((num = read(STDIN_FILENO, letter, 1)) > 0); bufsize++)
-		{
-			if (bufsize == 0)
-				line = _calloc(bufsize + 3, sizeof(char));
-			else
-				line = _realloc(line, bufsize, bufsize + 3);
-			if (!line)
-			{
-				num = 0;
-				break;
-			}
-			line[bufsize] = letter[0], line[bufsize + 1] = '\n';
-			line[bufsize + 2] = '\0';
-			if (line[bufsize] == '\n')
-				break;
-		}
-		if (num == 0 && bufsize == 0)
-			break;
-		else if (num == 0 && bufsize != 0)
-		{
-			num = 2;
-			break;
-		}
-		else if (line[0] != '\n')
-			return (line);
+		perror("fstat");
+		exit(-1);
 	}
-	if (num == 0)
-		salida1(m, e);
-	return (line);
-}
-
-/**
- * _getlineav - function to read what the user writes
- * @a: pointer to loop counter
- * @e: length of m
- * @m: copy of environmental
- * @av: arguments in input
- * Return: line in sucess otherwise NULL.
- */
-char  *_getlineav(int *a, char **m, int e, char **av)
-{
-	char letter[1] = {0}, *li = NULL;
-	size_t bz = 0;
-	static unsigned int num = 1;
-	static int  fd;
-
-	for (; (num != 0); bz = 0, free(li))
+	if ((sb.st_mode & S_IFMT) != S_IFIFO)
+		_strprint(PROMPT);
+	for (i = 0; i < size - 1; i++)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			close(fd), free_grid(m, e);
-			write(STDERR_FILENO, av[0], _strlen(av[0]));
-			write(STDERR_FILENO, ": 0: ", 5);
-			write(STDERR_FILENO, "Can't open ", 11);
-			write(STDERR_FILENO, av[1], _strlen(av[1]));
-			write(STDERR_FILENO, "\n", 1), exit(127);
-		}
-		*a = *a + 1;
-		while ((num = read(fd, letter, 1)) > 0)
-		{
-			if (bz == 0)
-				li = _calloc(bz + 3, sizeof(char));
-			else
-				li = _realloc(li, bz, bz + 3);
-			if (!li)
-			{
-				num = 0;
-				break;
-			}
-			li[bz] = letter[0], li[bz + 1] = '\n';
-			li[bz + 2] = '\0', bz++;
-		}
-		if (num == 0 && bz == 0)
+		readval = read(STDIN_FILENO, (input + i), 1);
+		if (readval == 0)
+			return (-1); /* EOF  */
+		if (input[i] == '\n')
 			break;
-		else if (li[0] != '\n')
-			return (li);
 	}
-	if (num == 0)
-		close(fd), salida3(m, e);
-	return (li);
+	input[i] = '\0';
+	return (i);
+}
+/**
+ * exit_shell - if user types 'exit' exit shell
+ * @line_tok: pointer to a tokenized input line
+ * Return: 1 if want to exit, 0 otherwise
+ */
+int exit_shell(char **line_tok)
+{
+	int cmp, lend, lline, size;
+
+	size = arr_size(line_tok) < 3;
+	cmp = _strncmp(line_tok[0], END, _strlen(END));
+	lend = _strlen(END);
+	lline = _strlen(line_tok[0]);
+	if (cmp == 0 && lend == lline && size)
+		return (1);
+	return (0);
+}
+/**
+ * clear_buffer - sets all the chars in buffer to 0
+ * @buffer: buffer to reset
+ * Return: nothing
+ */
+void clear_buffer(char *buffer)
+{
+	int i;
+
+	for (i = 0; i < BUFF_SIZE; i++)
+		buffer[i] = 0;
+}
+/**
+ * mem_cpy - copies memory area
+ * @dest: memory area to copy to
+ * @src: memory area copying from
+ * @n: how many bytes to copy
+ * Return: Pointer to dest
+ */
+char *mem_cpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated with a call to malloc
+ * @old_size: size, in bytes, of the allocated space for ptr
+ * @new_size: new size, in bytes of the new memory block
+ *
+ * Return: returns pointer to new memory location
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *array, *helper;
+	unsigned int i;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		array = malloc(new_size);
+		if (array == NULL)
+			return (NULL);
+		return (array);
+	}
+	array = malloc(sizeof(char) * new_size);
+	if (array == NULL)
+		return (NULL);
+	if (new_size > old_size && old_size != 0)
+	{
+		helper = ptr;
+		for (i = 0; i < old_size; i++)
+			array[i] = helper[i];
+		free(ptr);
+	}
+	if (new_size < old_size)
+	{
+		helper = ptr;
+		for (i = 0; i < new_size; i++)
+			array[i] = helper[i];
+		free(ptr);
+	}
+	return (array);
 }
--- shell.h
+++ shell.h
@@ -1,90 +1,101 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <fcntl.h>
+#ifndef SHELL_H
+#define SHELL_H
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-
-
-/*global variables*/
-
-char **create_env(char *envp[]);
-void _updateoldpwd(char *buf, char **myenv);
-void _updatepwd(char *buf, char **myenv);
-
-/*setenv*/
-int _issetenv(char **p, char ***myenv, int *e, int loop, char *v[]);
-void _setenv(char **p, char ***myenv, int *e, int loop, char *v[]);
-void _setenvcreat(char ***myenv, int *e, char *entirenv);
-
-/* unsetenv*/
-int _isunsetenv(char **p, char **myenv, int *e, int loop, char *v[]);
-void _unsetenv(char **p, char **myenv, int *e, int loop, char *v[]);
-void _errorenv(char **p);
-
-/* shell functions*/
-
-void _noargv(char *argv[], char *envp[]);
-void _yesargv(char *argv[], char *envp[]);
-void functions(char *line, int loop, char *argv[], char ***m, int *e, char *f);
-int rev(char **p, int L, char *li, char **v, char ***m, int *e, char *f);
-char *str_concat(char *s1, char *s2);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <stdint.h>
+#define BUFF_SIZE 1024
+#define PROMPT "$ "
+/* error messages */
+#define EINVAL "Invalid argument"
+#define ENOMEM "Out of memory"
+#define ERROR "Error"
+#define WRONG "Something went wrong"
+#define END "exit"
+/**
+ * struct built_in - list of builtins
+ * @bi: The specifier
+ * @f: The function associated with printing
+ */
+typedef struct built_in
+{
+	char *bi;
+	int (*f)();
+} builtin_t;
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+extern char **environ;
+/* lists.c */
+size_t list_len(list_t *h);
+list_t *add_node(list_t **head, char *str);
+list_t *add_node_end(list_t **head, char *str);
+void free_list(list_t *head);
+list_t *get_node(list_t **head, char *str);
+/* prints.c */
+int _putchar(char c);
+void _strprint(char *str);
+void print_array(char **array);
+size_t print_list(list_t *h);
+/* strings.c */
 int _strlen(char *s);
-void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-char *_getline(int *a, char **m, int e);
-char  *_getlineav(int *a, char **m, int e, char **av);
-char **parsing(char *line);
-char *_comments(char *line);
-int semicolon(char *line, int loop, char **argv);
-int currentstatus(int *status);
-
-/*char _getline_av(char *buffer);*/
-void free_grid(char **grid, int height);
-void _frk(char **p, char *l, int a, int L, char **v, int e, char **m, char *f);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char **checkbin(char **b, char **m);
-void *_realloc2(char *a, char *p, unsigned int old, unsigned int new_size);
-int  _isexit(char **p, int L, char *l, char **v, char **m, char *f);
-void _signal(int s);
-int _isenv(char **p, char **myenv);
-void _env(char **myenv);
-void _cd(char **a, int loop, char *v[], char **myenv);
-int _iscd(char **p, int loop, char *v[], char **myenv);
-
-char *_gethome(char **m);
-char *_changepwd(void);
-char *_changeoldpwd(void);
-char *_getpwd(char **m);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+/* more_strings */
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *str, char c);
+int len_to_char(char *str, char c);
 int _atoi(char *s);
-char *_strtoky(char *s, char *d);
-char *_strtoky2(char *s, char *d);
-
-#define SIZE 1024
-
-/* help files*/
-int _ishelp(char **p, int loop, char *v[], char **m);
-void _help_builtin(char **p, int loop, char *v[], char **m);
-void _help(char **p, int loop, char *v[], char **m);
-ssize_t read_help(char **m);
-ssize_t read_cdhelp(char **m);
-ssize_t read_exithelp(char **m);
-ssize_t read_helphelp(char **m);
-
-
-/* errors */
-void _put_err(char **p, int loop, int sig, char *v[]);
-void _builtinerr(char **p);
-void _builtinerr2(char **p);
-void _errorcd(char **p);
-void _errorexit(char **p);
-void _errorhelp(char **p);
-void _errorgarbage(char **p);
-void print_number(int n);
-
-
+/* _strtok.c */
+int count_words(char *str, char delim);
+int _wrdlen(char *s, char delim);
+char **strtow(char *str, char delim);
+/*  array_list.c */
+int arr_size(char **arr);
+list_t *array_to_list(char **array);
+char **list_to_array(list_t **head);
+void free_array(char **array);
+/* new_env.c */
+char *_getenv(char *name, list_t **env_head);
+int _setenv(char *name, char *value, list_t **env_head);
+int _unsetenv(char *name, list_t **env_head);
+int delete_node(list_t **head, char *string);
+char *var_str(char *name, char *value);
+/* _getline.c */
+int _getline(char *input, int size);
+int exit_shell(char **line_tok);
+void clear_buffer(char *buffer);
+char *mem_cpy(char *dest, char *src, int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/* cmd_line_loop.c */
+int cmd_line_loop(char *buffer, char *line, list_t **env_head);
+/* run_command */
+char **path_dirs_array(list_t **env_head);
+char *cmd_in_path(char *str, list_t **env_head);
+int run_command(char **line, list_t **env_head);
+/* built_ins.c */
+int built_ins(char **input, list_t **env_head);
+int exit_bi(char **line);
+int print_env(char **line, list_t **env_head);
+int set_env(char **line, list_t **env_head);
+int unset_env(char **line, list_t **env_head);
 #endif
